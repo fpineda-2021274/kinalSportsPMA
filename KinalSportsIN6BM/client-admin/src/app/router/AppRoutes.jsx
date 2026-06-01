45b080a59b8e448df0a5b4223c0497541f423754
fpineda-2021274 <fpineda-2021274@kinal.edu.gt>
2026-06-01 10:23:26 -0600

    fix:agregando cliente admin de KinalSportsPMA

---
+++ KinalSportsIN6BM/client-admin/src/app/router/AppRoutes.jsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+import { AuthPage } from "../../features/auth/pages/AuthPage.jsx";
+import { DashboardPage } from "../layouts/DashboardPage.jsx";
+import { Users } from "../../features/users/components/Users.jsx";
+import { Fields } from "../../features/fields/components/Fields.jsx";
+
+export const AppRoutes = () => {
+    return (
+        <Routes>
+            <Route path="/" element={<AuthPage />} />
+            <Route path="/dashboard" element={<DashboardPage />} >
+                <Route path="users" element={<Users />}/>
+                <Route path="fields" element={<Fields />} />
+            </Route>
+        </Routes>
+    )
+}
